5667bc3242ae5511554942a978c8b800f9694d38
OsatohanmwenT <osarenkhoeosato45@gmail.com>
2024-12-14 16:31:32 +0100

    budget category updated

--- components/BudgetCard.tsx
+++ components/BudgetCard.tsx
@@ -2,14 +2,18 @@ import React from 'react'
 import Link from "next/link";
 import {BudgetCategory} from "@/lib/entities";
 import {format} from "date-fns";
+import {formatNumber} from "@/lib/utils";
 
 const BudgetCard = ({ category_name, amount_limit, amount_used, start_date, end_date }: BudgetCategory) => {
     const remainingPercentage = amount_limit
         ? Math.max(0, 100 - ((amount_limit - amount_used) / amount_limit) * 100)
         : 0;
 
+    const remainingAmount = amount_limit && amount_limit - amount_used > 0 ?
+        amount_limit - amount_used : 0
+
     return (
-        <Link className="hover:shadow-xl transition-all rounded-lg" href={`/budget/category_budget/${category_name}`}>
+        <Link className="hover:shadow-xl h-fit transition-all rounded-lg" href={`/budget/category_budget/${category_name}`}>
             <div className="border-[1px] font-work-sans py-5 px-3 rounded-lg">
                 <div className="flex-between">
                     <p className="font-semibold font-work-sans text-lg">{category_name}</p>
@@ -18,9 +22,9 @@ const BudgetCard = ({ category_name, amount_limit, amount_used, start_date, end_
                 <div className="mt-2">
                     <div className="flex-between text-sm text-neutral-400 mb-3">
                         <p>€{amount_used} spent</p>
-                        <p>€{amount_limit - amount_used} remaining</p>
+                        <p>€{remainingAmount} remaining</p>
                     </div>
-                    <div className="w-full relative h-[6px] bg-light-green-100 rounded-lg">
+                    <div className="w-full overflow-hidden relative h-[6px] bg-light-green-100 rounded-lg">
                         <div style={{width: `${remainingPercentage}%`}}
                              className="h-full rounded-lg bg-green-200 w-4/6 hover:w-full transition-all duration-500"></div>
                     </div>
@@ -28,6 +32,11 @@ const BudgetCard = ({ category_name, amount_limit, amount_used, start_date, end_
                         <p>from {format(start_date,"PP")}</p>
                         <p>to {format(end_date, "PP")}</p>
                     </div>
+                    {amount_used > amount_limit &&
+                        <div className="flex items-center">
+                            <p className="text-red-400">Overspent by: £{formatNumber(amount_used-amount_limit)}</p>
+                        </div>
+                    }
                 </div>
             </div>
         </Link>
