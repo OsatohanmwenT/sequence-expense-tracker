42f1ce1deb2840f3a9925c0b4f9b0f8407369447
OsatohanmwenT <osarenkhoeosato45@gmail.com>
2024-12-14 13:29:38 +0100

    budget category updated

--- components/BudgetCard.tsx
+++ components/BudgetCard.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Link from "next/link";
 import {BudgetCategory} from "@/lib/entities";
+import {format} from "date-fns";
 
-const BudgetCard = ({ category_name, amount_limit, amount_used }: BudgetCategory) => {
+const BudgetCard = ({ category_name, amount_limit, amount_used, start_date, end_date }: BudgetCategory) => {
     const remainingPercentage = amount_limit
         ? Math.max(0, 100 - ((amount_limit - amount_used) / amount_limit) * 100)
         : 0;
@@ -15,12 +16,17 @@ const BudgetCard = ({ category_name, amount_limit, amount_used }: BudgetCategory
                     <p className="text-neutral-600 font-semibold">€{amount_limit}</p>
                 </div>
                 <div className="mt-2">
-                    <div className="flex-between text-sm text-neutral-400 mb-2">
+                    <div className="flex-between text-sm text-neutral-400 mb-3">
                         <p>€{amount_used} spent</p>
                         <p>€{amount_limit - amount_used} remaining</p>
                     </div>
                     <div className="w-full relative h-[6px] bg-light-green-100 rounded-lg">
-                        <div style={{ width: `${remainingPercentage}%` }} className="h-full rounded-lg bg-green-200 w-4/6 hover:w-full transition-all duration-500"></div>
+                        <div style={{width: `${remainingPercentage}%`}}
+                             className="h-full rounded-lg bg-green-200 w-4/6 hover:w-full transition-all duration-500"></div>
+                    </div>
+                    <div className="flex-between text-sm text-neutral-400 mt-5">
+                        <p>from {format(start_date,"PP")}</p>
+                        <p>to {format(end_date, "PP")}</p>
                     </div>
                 </div>
             </div>
