ca58f8b3d96c98b1a7fbf0dd6a4461c3c8f4bc6f
OsatohanmwenT <osarenkhoeosato45@gmail.com>
2024-12-15 16:17:28 +0100

    updated auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -10,7 +10,7 @@ const Layout = async ({ children }: {children: React.ReactNode}) => {
     return (
         <div>
             <div className="max-w-[400px] px-4 mx-auto">
-              <h1 className="text-center font-work-sans text-green font-semibold text-3xl my-8 sm:my-14">Sequence</h1>
+              <h1 className="text-center font-work-sans text-green font-semibold text-3xl my-3 sm:my-10">Sequence</h1>
               {children}
             </div>
         </div>
